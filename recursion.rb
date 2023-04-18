a1dce18f919956d8a7f62f216e21785544cb15ae
Nigel Aung-Myint <nigel.aungmyint@gmail.com>
2023-04-18 15:07:04 -0500

    add power solution

--- recursion.rb
+++ recursion.rb
@@ -41,3 +41,36 @@ def reverse(s)
   return s if s.length <= 1 # base case
   reverse(s[1..-1]) + s[0] # recursive case
 end
+
+# // create a function which takes a number and an exponent and
+# // recursively calls itself to calculate the product
+
+# // e.g.
+# let base = 2;
+# let exponent = 4;
+# let product = power(base, exponent)  // 2 to the 4th power
+
+# console.log(product);  // 16
+
+# psuedocode
+# base case:
+# return when expoenent reaches 0
+
+# recursive case: 
+# take the base and multiply by the function itself with the exponent less 1
+
+def power(base,exponent)
+  if exponent == 0
+    return 1
+  elsif exponent == 1
+    return base
+  elsif exponent < 0
+    return 1.0 / power(base, -exponent)
+  else
+    return base * power(base, exponent -1)
+  #   power(2,4) => 2 * power(2,3)
+  #                       2 * power(2,2)
+  #                               2 * power(2,1)
+  #                                       2
+  end
+end
--- recursion_spec.rb
+++ recursion_spec.rb
@@ -30,4 +30,38 @@ RSpec.describe 'recursion exercises' do
       expect(reverse(string)).to eq(solution)
     end
   end
+
+  describe '#power' do
+    it 'calculates the nth power of the base given a base and an exponent n' do
+      base = 2
+      exponent = 4
+      solution = 16
+
+      expect(power(base, exponent)).to eq(solution)
+    end
+
+    it 'calculates for power of 1' do
+      base = 2
+      exponent = 1
+      solution = 2
+
+      expect(power(base, exponent)).to eq(solution)
+    end
+
+    it 'calculates for power of 0' do
+      base = 0
+      exponent = 0
+      solution = 1
+
+      expect(power(base, exponent)).to eq(solution)
+    end
+
+    it 'calculates for negative powers' do
+      base = 3
+      exponent = -1
+      solution = (1.0/3)
+
+      expect(power(base, exponent)).to eq(solution)
+    end
+  end
 end
